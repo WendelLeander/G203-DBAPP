4fb0969cd1a84becf42844dd33144728ae4e0fb5
steamebanz_ <evanpinca@gmail.com>
2024-11-22 15:44:09 +0800

    Update athlete_management_menu.java

--- G203DBAPP/athlete_management_menu.java
+++ G203DBAPP/athlete_management_menu.java
@@ -10,8 +10,7 @@ public class athlete_management_menu {
         int menuselection = 0;
         Scanner console = new Scanner(System.in);
 
-        System.out.println("  ");
-        System.out.println("  ");
+        // Simplified Athlete Management Menu
         System.out.println("=======================================================");
         System.out.println("    Athlete Management Menu                           ");
         System.out.println("-------------------------------------------------------");
@@ -19,11 +18,10 @@ public class athlete_management_menu {
         System.out.println("[2] Update an Athlete Record                         ");
         System.out.println("[3] Delete an Athlete Record                         ");
         System.out.println("[4] View an Athlete Record and Current Team          ");
-        System.out.println("[5] View Performance History of an Athlete           ");
         System.out.println("[0] Exit Athlete Management                          ");
         System.out.println("=======================================================");
 
-        System.out.println("Enter Selected Function: ");
+        System.out.print("Enter Selected Function: ");
         menuselection = Integer.parseInt(console.nextLine());
 
         if (menuselection == 1) {
@@ -31,15 +29,15 @@ public class athlete_management_menu {
 
             System.out.println("Enter athlete profile");
             System.out.println("-------------------------------------------------------------------");
-            System.out.println("Athlete ID         : ");
+            System.out.print("Athlete ID         : ");
             a.athleteId = console.nextLine();
-            System.out.println("First Name         : ");
+            System.out.print("First Name         : ");
             a.firstName = console.nextLine();
-            System.out.println("Last Name          : ");
+            System.out.print("Last Name          : ");
             a.lastName = console.nextLine();
-            System.out.println("Birthday (YYYY-MM-DD) : ");
+            System.out.print("Birthday (YYYY-MM-DD) : ");
             a.birthday = console.nextLine();
-            System.out.println("Gender             : ");
+            System.out.print("Gender             : ");
             a.gender = console.nextLine().charAt(0);
 
             a.add_athlete();
@@ -48,7 +46,7 @@ public class athlete_management_menu {
             athlete_management a = new athlete_management();
 
             System.out.println("Enter athlete profile");
-            System.out.println("Athlete ID         : ");
+            System.out.print("Athlete ID         : ");
             a.athleteId = console.nextLine();
 
             if (a.view_athlete() == 0) {
@@ -64,13 +62,13 @@ public class athlete_management_menu {
 
                 System.out.println("\nEnter updated athlete profile");
                 System.out.println("-------------------------------------------------------------------");
-                System.out.println("First Name         : ");
+                System.out.print("First Name         : ");
                 a.firstName = console.nextLine();
-                System.out.println("Last Name          : ");
+                System.out.print("Last Name          : ");
                 a.lastName = console.nextLine();
-                System.out.println("Birthday (YYYY-MM-DD): ");
+                System.out.print("Birthday (YYYY-MM-DD): ");
                 a.birthday = console.nextLine();
-                System.out.println("Gender             : ");
+                System.out.print("Gender             : ");
                 a.gender = console.nextLine().charAt(0);
 
                 a.update_athlete();
@@ -79,7 +77,7 @@ public class athlete_management_menu {
             athlete_management a = new athlete_management();
 
             System.out.println("Enter athlete profile");
-            System.out.println("Athlete ID         : ");
+            System.out.print("Athlete ID         : ");
             a.athleteId = console.nextLine();
 
             a.delete_athlete();
@@ -88,24 +86,25 @@ public class athlete_management_menu {
             athlete_management a = new athlete_management();
 
             System.out.println("Enter athlete profile");
-            System.out.println("Athlete ID         : ");
+            System.out.print("Athlete ID         : ");
             a.athleteId = console.nextLine();
 
             if (a.view_athlete() == 0) {
                 System.out.println("Athlete does not exist in the records");
             } else {
-                System.out.println("Athlete Record and Team Retrieved");
-            }
-
-        } else if (menuselection == 5) {
-            athlete_management a = new athlete_management();
-
-            System.out.println("Enter athlete profile");
-            System.out.println("Athlete ID         : ");
-            a.athleteId = console.nextLine();
-
-            if (a.track_performance_history() == 0) {
-                System.out.println("No performance history found for the athlete");
+                // Displaying the retrieved athlete information
+                System.out.println("Athlete Information:");
+                System.out.println("ID: " + a.athleteId);
+                System.out.println("Name: " + a.firstName + " " + a.lastName);
+                System.out.println("Birthday: " + a.birthday);
+                System.out.println("Gender: " + a.gender);
+                
+                // Showing the current team information
+                if (a.teamName != null) {
+                    System.out.println("Current Team: " + a.teamName);
+                } else {
+                    System.out.println("No current team assigned.");
+                }
             }
         }
 
@@ -113,7 +112,6 @@ public class athlete_management_menu {
     }
 
     public static void main(String args[]) {
-
         athlete_management_menu amm = new athlete_management_menu();
         while (amm.menu() != 0) {
         }
