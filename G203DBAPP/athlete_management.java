c0f3b4ff3ededc3de846b7906d92bf338c42a9bf
steamebanz_ <evanpinca@gmail.com>
2024-11-22 16:33:08 +0800

    Update athlete_management.java

--- G203DBAPP/athlete_management.java
+++ G203DBAPP/athlete_management.java
@@ -1,3 +1,4 @@
+// 11-22-2024: 4:32 PM
 package G203DBAPP;
 import java.sql.*;
 
@@ -10,7 +11,6 @@ public class athlete_management {
     public char gender;
     public String teamName;
 
-    // Constructor
     public athlete_management() {
         athleteId = "";
         firstName = "";
@@ -20,7 +20,6 @@ public class athlete_management {
         teamName = "";
     }
 
-    // Add new athlete to the database
     public int add_athlete() {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=p@ssword");
@@ -28,10 +27,9 @@ public class athlete_management {
             pstmt.setString(1, athleteId);
             pstmt.setString(2, firstName);
             pstmt.setString(3, lastName);
-            pstmt.setString(4, birthday); // Storing birthday
+            pstmt.setString(4, birthday);
             pstmt.setString(5, String.valueOf(gender));
             pstmt.executeUpdate();
-            System.out.println("Athlete record was created");
             pstmt.close();
             conn.close();
             return 1;
@@ -41,18 +39,16 @@ public class athlete_management {
         }
     }
 
-    // Update existing athlete in the database
     public int update_athlete() {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=p@ssword");
             PreparedStatement pstmt = conn.prepareStatement("UPDATE athletes SET firstName=?, lastName=?, birthday=?, gender=? WHERE athleteID=?");
             pstmt.setString(1, firstName);
             pstmt.setString(2, lastName);
-            pstmt.setString(3, birthday); // Storing updated birthday
+            pstmt.setString(3, birthday);
             pstmt.setString(4, String.valueOf(gender));
             pstmt.setString(5, athleteId);
             pstmt.executeUpdate();
-            System.out.println("Athlete record was updated");
             pstmt.close();
             conn.close();
             return 1;
@@ -62,14 +58,12 @@ public class athlete_management {
         }
     }
 
-    // Delete athlete record from the database
     public int delete_athlete() {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=p@ssword");
             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM athletes WHERE athleteID=?");
             pstmt.setString(1, athleteId);
             pstmt.executeUpdate();
-            System.out.println("Athlete record was deleted");
             pstmt.close();
             conn.close();
             return 1;
@@ -79,7 +73,6 @@ public class athlete_management {
         }
     }
 
-    // View athlete record along with their current team
     public int view_athlete() {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=p@ssword");
@@ -87,30 +80,51 @@ public class athlete_management {
             pstmt.setString(1, athleteId);
             ResultSet rs = pstmt.executeQuery();
 
-            // Fetching athlete details
             while (rs.next()) {
                 firstName = rs.getString("firstName");
                 lastName = rs.getString("lastName");
-                birthday = rs.getString("birthday"); 
+                birthday = rs.getString("birthday");
                 gender = rs.getString("gender").charAt(0);
 
-                System.out.println("Athlete Information:");
-                System.out.println("ID: " + athleteId);
-                System.out.println("Name: " + firstName + " " + lastName);
-                System.out.println("Birthday: " + birthday); 
-                System.out.println("Gender: " + gender);
-
-                // Fetching the current team of the athlete
                 PreparedStatement pstmt2 = conn.prepareStatement("SELECT teamName FROM athlete_teams WHERE athleteID=?");
                 pstmt2.setString(1, athleteId);
                 ResultSet rs2 = pstmt2.executeQuery();
                 if (rs2.next()) {
                     teamName = rs2.getString("teamName");
-                    System.out.println("Current Team: " + teamName);
                 } else {
-                    System.out.println("No current team assigned.");
+                    teamName = "No current team assigned.";
                 }
                 rs2.close();
+            }
+
+            pstmt.close();
+            conn.close();
+            return 1;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int view_performance_history() {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbathletes?useTimezone=true&serverTimezone=UTC&user=root&password=p@ssword");
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM performance WHERE athleteID=?");
+            pstmt.setString(1, athleteId);
+            ResultSet rs = pstmt.executeQuery();
+
+            boolean hasPerformance = false;
+            while (rs.next()) {
+                hasPerformance = true;
+                String date = rs.getString("date");
+                String performance = rs.getString("performance");
+                System.out.println("Date: " + date);
+                System.out.println("Performance: " + performance);
+            }
+
+            if (!hasPerformance) {
+                System.out.println("No performance records found.");
             }
 
             pstmt.close();
